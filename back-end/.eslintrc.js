1ec282d032bbe315d15c147bfd15fa946b986b2a
hos101010 <hos101010@naver.com>
2020-06-30 05:41:28 +0900

    :wrench: eslint 적용

---
+++ back-end/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["airbnb-base", "plugin:@typescript-eslint/eslint-recommended"],
+  rules: {
+    "linebreak-style": 0,
+    quotes: [2, "double", { avoidEscape: false }],
+    "comma-dangle": 0,
+    "import/no-unresolved": 0,
+    "import/extensions": 0,
+    "implicit-arrow-linebreak": 0,
+  },
+};
--- back-end/src/www.ts
+++ back-end/src/www.ts
@@ -1,9 +1,11 @@
-import 'source-map-support/register'; // source-map을 사용하기 위해 추가함.
-import App from './App';
+import "source-map-support/register";
+// eslint-disable-next-line no-unused-vars
 import * as express from "express";
+import App from "./App";
 
 const port: number = Number(process.env.PORT) || 3001;
 const app: express.Application = new App().app;
 
-app.listen(port, () => console.log(`Express server listening at ${port}`))
-.on('error', err => console.error(err));
+app
+  .listen(port, () => console.log(`Express server listening at ${port}`))
+  .on("error", (err) => console.error(err));
